facd222c449f890fffcd85d32d4819d87a7b7b3f
Andy Parsons <a.d.parsons@btinternet.com>
2021-01-11 13:24:23 +0000

    Implementing lights on when getting home

--- Dawn_Server/watcher.py
+++ Dawn_Server/watcher.py
@@ -1,34 +1,64 @@
 import os
 import time
+import threading
+import RPi.GPIO as GPIO
 from music_handler import MusicHandler
 from warm_glow import LightHandler
 
 RECOGNISED_PHONE_IPS = ['192.168.1.213',  # Parsons Fairphone
                         '192.168.1.130']  # Sophs Fairphone
+SENSOR = 16
 
 class PhoneWatch:
     light_handler = ...  # type: LightHandler
     music_handler = ...  # type: MusicHandler
 
     def __init__(self, music_handler, light_handler):
+        GPIO.setup(SENSOR, GPIO.IN)
         self.music_handler = music_handler
         self.light_handler = light_handler
         self.had_connection_before = True
+        self.is_anyone_home = True
+
+    def returned_home(self):
+        for i in range(60):
+            if GPIO.input(SENSOR):
+                self.light_handler.toggle_kitchen_lights()
+                break
+            else:
+                time.sleep(10)
 
     def watch(self):
         while True:
-            time.sleep(30 * 60)
-            any_phones_found = False
-            for ip in RECOGNISED_PHONE_IPS:
-                response = os.system("ping -c 1 " + ip)
-                # and then check the response...
-                if response == 0:
-                    any_phones_found = True
-                    break
-            if any_phones_found:
-                self.had_connection_before = True
+            if self.is_anyone_home:
+                time.sleep(30 * 60)
+                any_phones_found = False
+                for ip in RECOGNISED_PHONE_IPS:
+                    response = os.system("ping -c 1 " + ip)
+                    # and then check the response...
+                    if response == 0:
+                        any_phones_found = True
+                        break
+                if any_phones_found:
+                    self.had_connection_before = True
+                else:
+                    if not self.had_connection_before:
+                        self.music_handler.turn_radio_off()
+                        self.light_handler.kill_all()
+                        self.is_anyone_home = False
+                    self.had_connection_before = False
             else:
-                if not self.had_connection_before:
-                    self.music_handler.turn_radio_off()
-                    self.light_handler.kill_all()
-                self.had_connection_before = False
+                time.sleep(30)
+                any_phones_found = False
+                for ip in RECOGNISED_PHONE_IPS:
+                    response = os.system("ping -c 1 " + ip)
+                    # and then check the response...
+                    if response == 0:
+                        any_phones_found = True
+                        break
+                if any_phones_found:
+                    self.is_anyone_home = True
+                    check_for_movement = threading.Thread(target=self.returned_home)
+                    check_for_movement.start()
+
+
